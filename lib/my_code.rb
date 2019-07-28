5608c3aad6977edd067dae26fb19d0bd70831811
Damon Self <damongself@gmail.com>
2019-07-28 15:11:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,7 @@ def reduce_to_all_true(source_array)
   i = 0
   total = 0
   while i < source_array.length do
-    if source_array[i] == true
+    if source_array[i]
       total += 1
       i += 1
     elsif total == source_array.length
