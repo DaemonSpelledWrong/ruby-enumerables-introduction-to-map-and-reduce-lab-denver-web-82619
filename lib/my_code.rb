998a7d32a7cb1475f6afb18d301ddeef0b3981d0
Damon Self <damongself@gmail.com>
2019-07-28 15:56:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -67,7 +67,7 @@ end
 def reduce_to_any_true(source_array)
   i = 0
     while i < source_array.length do
-    if source_array[i].falsey? 
+    if source_array[i]
       i += 1
     elsif i == source_array.length
       return false
